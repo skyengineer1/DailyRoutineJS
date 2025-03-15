41ec808f6b859ac3faec0939813ee3152a40d846
Sky <skyengineer001@gmail.com>
2025-03-15 22:12:48 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+day = 'Sunday';
+
+switch (day) {
+    case 'Monday':
+        console.log('Eaty, sleepy, trainy!');
+        break;
+    case 'Tuesday':
+        console.log('Eaty, sleepy, trainy!');
+        break;
+    case 'Wednesday':
+        console.log('Eaty, sleepy, trainy!');
+        break;
+    case 'Thursday':
+        console.log('Eaty, sleepy, trainy!');
+        break;
+    case 'Friday':
+        console.log('Eaty, sleepy, trainy!');
+        break;
+    case 'Saturday':
+        console.log('Eaty, sleepy, trainy!');
+        break;
+    case 'Sunday':
+        console.log('Rest!');
+        break;
+}
